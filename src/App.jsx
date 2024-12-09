af0106ef4e48483d46179fe87b66bc96cfb34e4e
Aanand-Patil <anandpatil6440@gmail.com>
2024-12-09 20:32:18 +0530

    testimonials component added.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <FeatureSection />
         <Workflow />
         <Pricing />
+        <Testimonials />
       </div>
     </>
   );
